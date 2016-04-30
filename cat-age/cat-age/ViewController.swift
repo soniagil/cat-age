bc8f697c9c31db4ccc0c7f1316b262309db14680
Sonia Gil <sgilbenitez@gmail.com>
2016-04-30 20:14:42 +0200

    adding base project

---
+++ cat-age/cat-age/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  cat-age
+//
+//  Created by Sonia Gil Benítez on 30/4/16.
+//  Copyright © 2016 Sonia Gil Benítez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
