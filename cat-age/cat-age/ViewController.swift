a812027ea810543ae1297b11756db233d5c5ebce
Sonia Gil <sgilbenitez@gmail.com>
2016-05-01 12:40:27 +0200

    add new field change design close keyboard when action has finished or has touched anywhere

--- cat-age/cat-age/ViewController.swift
+++ cat-age/cat-age/ViewController.swift
@@ -11,39 +11,55 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var btCalculate: UIButton!
+    @IBOutlet weak var txtCatName: UITextField!
     @IBOutlet weak var txtAge: UITextField!
     @IBOutlet weak var txtResult: UILabel!
     @IBOutlet weak var imageCat: UIImageView!
+    @IBOutlet weak var msgError: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         btCalculate.layer.cornerRadius = 5
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func dismissKeyboard() {
+        view.endEditing(true)
+    }
 
     @IBAction func calculate(sender: AnyObject) {
         let humanAge = Int(txtAge.text!)
+        let catName = txtCatName.text!
+        
         if humanAge != nil {
+            msgError.text = ""
+            
             let catYears = humanAge! * 7
-            
             let images = ["cat_grumpy", "cat_poo", "cat_acrobat", "cat_scared", "cat_sing"]
-            
             let randomNumber = arc4random_uniform(UInt32(images.count))
-            
             let imageName = images[Int(randomNumber)]
             
             imageCat.image = UIImage(named: imageName)
             
-            txtResult.text = String(catYears) + " años!"
+            if catName != "" {
+                txtResult.text = catName + " is " + String(catYears) + " years old!"
+            } else {
+                txtResult.text = String(catYears) + " years old!"
+            }
+            
         } else {
-            txtResult.text = "Por favor, introduzca la edad"
+            msgError.text = "Please, complete the age"
         }
+        
+        dismissKeyboard()
     }
 
 }
